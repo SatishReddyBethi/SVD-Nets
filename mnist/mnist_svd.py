0073762e9dbc63125d74295347bcbd417cc40016
Satish Bethi <satishmsid@gmail.com>
2023-09-02 06:56:51 -0600

    Finished first prototype

--- mnist/mnist_svd.py
+++ mnist/mnist_svd.py
@@ -9,104 +9,144 @@ from torch.optim.lr_scheduler import StepLR
 from tqdm import tqdm
 import time
 
-def train(args, train_loader, epoch):
-    # Set initial weights
+def estimate_activations(weights, ground_truth_list, num_classes):
+    unique_grouth_truths = torch.unique(ground_truth_list)
+    #print(f"unique_grouth_truths: {unique_grouth_truths}")
+    Z = {}
+    for y in unique_grouth_truths:
+        A_out = F.one_hot(y, num_classes=num_classes)
+        y_value = y.item()
+        Z[y_value] = {}
+        # TODO: Vectorize this for loop. Calculate Z for multiple y values at once.
+        for j in reversed(range(len(weights))):
+            # Iterates from last layer to first layer
+            Z_out = sigmoid_inverse(A_out)
+            # Save Z to a serperate list
+            Z[y_value][j] = Z_out
+            #print(f"Layer {j} => Z.shape: {Z_out.shape}")
+            A = torch.zeros(weights[j].shape[0])
+            for k in range(Z_out.shape[0]):
+                # Iterates through each node of a layer
+                if Z_out[k] > 0:
+                    # Maximize Z
+                    A = A + (weights[j][:,k] > 0)
+                else:
+                    # Minimize Z
+                    A = A + (weights[j][:,k] < 0)
+            # Save A as it's average from all the layers
+            A_out = A/Z_out.shape[0]
+    #print(Z.keys())
+    #print(Z[unique_grouth_truths[1].item()].keys())
+    return Z
+
+def calculate_weights(input, target, Z_est, principal_vecs, principal_vals, weights):
+    # Calculate weights using SVD
+    num_layers = len(weights)
+    batch_size = input.shape[0]
+    Z = {}
+    for layer in range(num_layers):
+        out_dim = weights[layer].shape[1]
+        Z[layer] = torch.zeros(out_dim,batch_size)
+        for i in range(batch_size):
+            # Calculate weights for each example
+            # TODO: Vectorize this for loop. Try to calculate weights for multiple targets at once.
+            Z[layer][:,i] = Z_est[target[i].item()][layer]
+
+    A = input
+    for layer in range(num_layers):
+        in_dim = weights[layer].shape[0]
+        out_dim = weights[layer].shape[1]
+        #print(f"A.shape: {A.shape}")
+        batch_vecs = A.view(1,batch_size,in_dim).repeat(out_dim,1,1)
+        batch_vals = Z[layer]
+        #print(f"principal_vecs[{layer}]: {principal_vecs[layer].shape}")
+        #print(f"principal_vals[{layer}]: {principal_vals[layer].shape}")
+        #print(f"batch_vecs[{layer}]: {batch_vecs.shape}")
+        #print(f"batch_vals[{layer}]: {batch_vals.shape}")
+        all_vecs = torch.cat((principal_vecs[layer],batch_vecs),dim=1)
+        all_vals = torch.cat((principal_vals[layer],batch_vals),dim=1)
+        #print(f"all_vecs[{layer}]: {all_vecs.shape}")
+        #print(f"all_vals[{layer}]: {all_vals.shape}")
+        for i in tqdm(range(all_vecs.shape[0])):
+            vecs = all_vecs[i,:,:]
+            vals = all_vals[i,:]
+            U, S, Vh = torch.linalg.svd(vecs, full_matrices=False)
+            sigma_rec = 1/S
+            weights[layer][:,i] = (Vh.T*sigma_rec)@U.T@vals
+            # Calculate prinicipal vecs and values
+            principal_vecs[layer][i,:,:] = S.reshape(S.shape[0],1)*Vh
+            principal_vals[layer][i,:] = U.T@vals
+        # Calculate A values for next layer
+        Z_new = A@weights[layer]
+        A = torch.sigmoid(Z_new)
+    
+    print(f"Final Output: {torch.argmax(A, dim=1)}")
+    print(f"Ground Truth: {target}")
+    return weights,principal_vecs,principal_vals
+
+
+def sigmoid_inverse(a:torch.Tensor):
+    # Clamp the a values between 1*10^-6 and 1-1*10^-6
+    return torch.special.logit(a, eps=1e-6) 
+
+def train(args, train_loader, epoch, test_loader):
+    # Do not use gradient calculation
+    torch.set_grad_enabled(False)
+    # Set initial weights between -1 and 1
     # We have to transpose the weight matrix to be able to multiply matries with batches
-    w1 = torch.rand(512,784).T
-    w2 = torch.rand(128,512).T
-    w3 = torch.rand(10,128).T
-    # TODO: Call equations as vectors. Eg: Principal vectors
+    weights = []
+    weights.append(2*torch.rand(784,512)-1)
+    weights.append(2*torch.rand(512,128)-1)
+    weights.append(2*torch.rand(128,10)-1)
+    
+    num_classes=weights[-1].shape[1]
     # Initialize eqautions for every node
     # Eg: For one node, they would be of shape (w.shape[0],w.shape[0])
     # Remember that shape of z is always (batch_size,w.shape[0],1)
-    principal_eqns1 = torch.zeros(w1.shape[1],w1.shape[0],w1.shape[0])
-    principal_eqns2 = torch.zeros(w2.shape[1],w2.shape[0],w2.shape[0])
-    principal_eqns3 = torch.zeros(w3.shape[1],w3.shape[0],w3.shape[0])
-    principal_vals1 = torch.zeros(w1.shape[1],w1.shape[0])
-    principal_vals2 = torch.zeros(w2.shape[1],w2.shape[0])
-    principal_vals3 = torch.zeros(w3.shape[1],w3.shape[0])
+    principal_vecs = []
+    principal_vals = []
+    for i in range(len(weights)):
+        principal_vecs.append(torch.zeros(weights[i].shape[1],weights[i].shape[0],weights[i].shape[0]))
+        principal_vals.append(torch.zeros(weights[i].shape[1],weights[i].shape[0]))
+    
+    old_data = torch.zeros(0,weights[0].shape[0])
+    old_target = torch.zeros(0)
     for batch_idx, (data, target) in enumerate(train_loader):
         data = torch.flatten(data, start_dim=1)
-        # Calculate forward pass and get equations for all the nodes
-        # input
-        a0 = data
-        print(f"a0_shape: {a0.shape}")
-        # layer 1
-        z1 = a0@w1
-        a1 = sigmoid(z1)
-        print(f"a1_shape: {a1.shape}")
-        # layer 2
-        z2 = a1@w2
-        a2 = sigmoid(z2)
-        print(f"a2_shape: {a2.shape}")
-        # layer 3
-        z3 = a2@w3
-        a3 = sigmoid(z3)
-        print(f"a3_shape: {a3.shape}")
-        print("==========================")
-        # Get new equations from all layers (batch_size,a.shape[1],z.shape[1]+z.shape[2])
-        # a.shape[1] is the size of output a (batch_size, a.shape[1])
-        layer1_eqns = a0
-        layer1_vals = z1.T
-        layer2_eqns = a1
-        layer2_vals = z2.T
-        layer3_eqns = a2
-        layer3_vals = z3.T
-        # Combine the new equations with principal equations
-        # TODO: Test torch.Tensor.repeat instead of multiplying by torch.ones
-        layer1_eqns = torch.ones(w1.shape[1],args.batch_size,w1.shape[0])*layer1_eqns.view(1,args.batch_size,w1.shape[0])
-        layer2_eqns = torch.ones(w2.shape[1],args.batch_size,w2.shape[0])*layer2_eqns.view(1,args.batch_size,w2.shape[0])
-        layer3_eqns = torch.ones(w3.shape[1],args.batch_size,w3.shape[0])*layer3_eqns.view(1,args.batch_size,w3.shape[0])
-        print(f"principal_eqns1: {principal_eqns1.shape}")
-        print(f"layer1_eqns: {layer1_eqns.shape}")
-        all_eqns1 = torch.cat((principal_eqns1,layer1_eqns),1)
-        all_eqns2 = torch.cat((principal_eqns2,layer2_eqns),1)
-        all_eqns3 = torch.cat((principal_eqns3,layer3_eqns),1)
-        print(f"all_eqns1: {all_eqns1.shape}\nall_eqns2: {all_eqns2.shape}\nall_eqns3: {all_eqns3.shape}")
-        print("==========================")
-        # Combine the new values with principal values
-        #layer1_vals = torch.ones(w1.shape[1],args.batch_size,w1.shape[0])*layer1_vals.view(1,args.batch_size,w1.shape[0])
-        #layer2_vals = torch.ones(w2.shape[1],args.batch_size,w2.shape[0])*layer2_eqns.view(1,args.batch_size,w2.shape[0])
-        #layer3_vals = torch.ones(w3.shape[1],args.batch_size,w3.shape[0])*layer3_eqns.view(1,args.batch_size,w3.shape[0])
-        print(f"principal_vals1: {principal_vals1.shape}")
-        print(f"layer1_vals: {layer1_vals.shape}")
-        all_vals1 = torch.cat((principal_vals1,layer1_vals),1)
-        all_vals2 = torch.cat((principal_vals2,layer2_vals),1)
-        all_vals3 = torch.cat((principal_vals3,layer3_vals),1)
-        print(f"all_vals1: {all_vals1.shape}\nall_vals2: {all_vals2.shape}\nall_vals3: {all_vals3.shape}")
-        print("==========================")
-        # Calculate all the parameters from equations using SVD
-        # Iterative implementation of SVD
-        st = time.time()
-        for i in tqdm(range(all_eqns1.shape[0])):
-            eqns = all_eqns1[i,:,:]
-            vals = all_vals1[i,:]
-            U, S, Vh = torch.linalg.svd(eqns, full_matrices=False)
-            sigma_rec = 1/S
-            w1[:,i] = (Vh.T*sigma_rec)@U.T@vals
-            # Calculate prinicipal eqns and values
-            principal_eqns1[i,:,:] = S*Vh
-            principal_vals1[i,:] = U.T@vals
-        
-        print(f"Iterative SVD Execution time: {time.time() - st} seconds")
+        Z_est = estimate_activations(weights, target, num_classes=num_classes)
+        weights,principal_vecs,principal_vals = calculate_weights(data, target, Z_est, principal_vecs, principal_vals, weights)
+        if (old_data.shape[0] > 0) and (old_target.shape[0] > 0):
+            pred = torch.argmax(forward_pass(old_data, weights), dim=1)
+            acc = torch.sum(pred == old_target)/old_data.shape[0]
+            print(f"Old Data Accuracy: {acc*100}% | No. of examples: {old_data.shape[0]}")
+            test(test_loader,weights)
 
-        # Vectorized implementation of SVD
-        st = time.time()
-        U, S, Vh = torch.linalg.svd(all_eqns1, full_matrices=False)
-        print(S[0,100])
-        S_new = S.reshape(all_eqns1.shape[0],1,S.shape[1])
-        Sigma_rec = 1/S_new
-        values = all_vals1.reshape(all_eqns1.shape[0],-1,1)
-        w1 = (Vh.permute(0,2,1) * Sigma_rec)@U.permute(0,2,1)@values
-        principal_eqns1 = S_new*Vh
-        principal_vals1 = U.permute(0,2,1)@values
-        print('Vectorized SVD Execution time:', time.time() - st, 'seconds')
-        # Replace the old parameters with the newly calculated parameters and principal equations
+        old_data = torch.cat([old_data, data],dim=0)
+        print(f"old_target.shape: {old_target.shape}, target.shape: {target.shape}")
+        old_target = torch.cat([old_target, target],dim=0)
         if args.dry_run:
             break
+    return weights,principal_vecs,principal_vals
 
-def sigmoid(x):
-    return 1/(1+torch.exp(-x))
+def forward_pass(input, weights):
+    A = input
+    for layer in range(len(weights)):
+        Z = A@weights[layer]
+        A = torch.sigmoid(Z)
+    return A
+
+def test(test_loader,weights):
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = torch.flatten(data, start_dim=1)
+
+            pred = forward_pass(data,weights).argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    print('\nTest Set Accuracy: {}/{} ({:.0f}%)\n'.format(
+        correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 def main():
     # Training settings
@@ -167,7 +207,8 @@ def main():
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     for epoch in range(1, args.epochs + 1):
-        train(args, train_loader, epoch)
+        weights,principal_vecs,principal_vals = train(args, train_loader, epoch, test_loader)
+        test(test_loader,weights)
         if args.dry_run:
             break
 
